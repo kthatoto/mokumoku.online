a2509fa674780d9557d66d315b57c31dfaa5c006
kthatoto <kthatoto@gmail.com>
2020-04-27 05:55:15 +0900

    Add default page

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,44 @@
 <template lang="pug">
-nuxt.default
+el-container.default
+  el-header.default__header
+    el-dropdown.dropdown(trigger="click" @command="handle")
+      el-button.dropdown__button
+        icon.icon(name="user")
+      el-dropdown-menu(slot="dropdown")
+        el-dropdown-item(command="signout") ログアウト
+  nuxt.root
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup (_, context: any) {
+    const signout = async () => {
+      await context.root.$firebase.auth().signOut()
+      context.root.$message({ message: 'ログアウトしました', type: 'success', duration: 5000 })
+      context.root.$router.push('/signin')
+    }
+    const handleCommand = (command: string) => {
+      if (command === 'signout') signout()
+    }
+
+    return { handleCommand }
+  }
+})
+</script>
+
+<style lang="stylus" scoped>
+.default
+  &__header
+    text-align: right
+    border-bottom: 1px solid borderColor
+    .dropdown
+      height: 100%
+      &__button
+        height: 100%
+        width: 100px
+        border-radius: 0
+        border-top: none
+        border-bottom: none
+</style>
