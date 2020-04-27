a283e5d9a258578ddf57e7b30eea8657109aaf67
kthatoto <kthatoto@gmail.com>
2020-04-27 11:24:36 +0900

    Setting for api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,18 @@
-import * as functions from 'firebase-functions'
 
 export const hello_world = functions.https.onRequest((req, res) => {
   res.send("Hello from Firebase!")
 })
+
+import * as functions from 'firebase-functions'
+import express from 'express'
+import cors from 'cors'
+
+const api = express()
+
+api.use(cors({ origin: true }))
+
+api.get('/messages', (req, res) => {
+  res.send(['foo', 'bar'])
+})
+
+export default functions.https.onRequest(api)
