4fef44f49ffe6a3540938819d627d6da7a36a3ec
kthatoto <kthatoto@gmail.com>
2020-05-17 06:50:50 +0900

    Add tag global atom component

---
+++ src/components/atoms/Tag.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+el-tag.tag(:closable="closable" @close="close")
+  icon.icon.-r(name="hashtag")
+  span {{ label }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+interface Props {
+  label: string
+  closable: boolean
+}
+
+export default defineComponent({
+  props: {
+    label: { type: String, required: true },
+    closable: { type: Boolean, required: false, default: false }
+  },
+  setup (props: Props, context) {
+    const close = () => { context.emit('close', props.label) }
+    return { close }
+  }
+})
+</script>
+
+<style lang="stylus" scoped>
+.tag
+  font-size: 12px
+  margin-bottom: 5px
+  background-color: color1
+  padding: 0 7px 0 5px
+  &:not(:last-child)
+    margin-right: 10px
+  .icon
+    vertical-align: -2px
+</style>
--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Link from '@/components/atoms/Link'
 import Avatar from '@/components/atoms/Avatar'
+import Tag from '@/components/atoms/Tag'
 
 Vue.component('Link', Link)
 Vue.component('Avatar', Avatar)
+Vue.component('Tag', Tag)
