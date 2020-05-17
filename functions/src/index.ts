8d20b78a8ddeeb21035eba4574359f5a4b60d90c
kthatoto <kthatoto@gmail.com>
2020-05-17 09:18:43 +0900

    Test functions trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,23 @@
 import * as functions from 'firebase-functions'
-import express from 'express'
-import cors from 'cors'
+// import express from 'express'
+// import cors from 'cors'
+//
+// const api = express()
+//
+// api.use(cors({ origin: true }))
+//
+// api.get('/messages', (_req, res) => {
+//   res.send(['foo', 'bar'])
+// })
+//
+// exports.api = functions.https.onRequest(api)
 
-const api = express()
+interface Event {
+  id: string
+  tags: string
+}
 
-api.use(cors({ origin: true }))
-
-api.get('/messages', (_req, res) => {
-  res.send(['foo', 'bar'])
-})
-
-exports.api = functions.https.onRequest(api)
+exports.tagTrigger = functions.firestore.document('events/{_eventId}')
+  .onWrite((change: any, context: any) => {
+    console.log(change)
+  })
