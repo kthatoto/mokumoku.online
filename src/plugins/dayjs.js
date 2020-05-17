76c4894a692b2169301df15543b70ff7174d6788
kthatoto <kthatoto@gmail.com>
2020-05-18 01:32:02 +0900

    Extend dayjs

--- src/plugins/dayjs.js
+++ src/plugins/dayjs.js
@@ -1,4 +1,6 @@
 import dayjs from 'dayjs'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 import 'dayjs/locale/ja'
 dayjs.locale('ja')
+dayjs.extend(isSameOrBefore)
 export default (_context, inject) => { inject('dayjs', dayjs) }
