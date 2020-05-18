5e7fdd44bc480cf8c833aa4eb58abe475a261497
kthatoto <kthatoto@gmail.com>
2020-05-19 07:00:36 +0900

    Re styling event card (wip)

--- src/components/molecules/EventCard.vue
+++ src/components/molecules/EventCard.vue
@@ -1,22 +1,39 @@
 <template lang="pug">
 el-card.event-card
-  nuxt-link.title(:to="'/mokumoku/' + event.id")
-    h3 {{ event.title }}
-  h4 詳細
-  p.description {{ event.description }}
-  h4 時間
-  p.datetime
-    span.date {{ event.dateString }}
-    span {{ event.startDatetime }}
-    span 〜
-    span {{ event.endDatetime }}
-  h4 参加ユーザー
-  .users
-    Avatar.user(v-for="u in event.users" :key="u.uid" :user="u")
+  .main
+    .time-info
+      .year {{ year }}
+      .month-day
+        span {{ month }}
+        span /
+        span {{ day }}
+        span ({{ weekChar }})
+    .main-info
+      .group
+      .title
+        nuxt-link(:to="'/mokumoku' + event.id")
+          h3 {{ event.title }}
+  .sub
+    .tags
+      Tag(v-for="tag in event.tags" :key="tag" :label="tag")
+    .description
+  //- nuxt-link.title(:to="'/mokumoku/' + event.id")
+  //-   h3 {{ event.title }}
+  //- h4 詳細
+  //- p.description {{ event.description }}
+  //- h4 時間
+  //- p.datetime
+  //-   span.date {{ event.dateString }}
+  //-   span {{ event.startDatetime }}
+  //-   span 〜
+  //-   span {{ event.endDatetime }}
+  //- h4 参加ユーザー
+  //- .users
+  //-   Avatar.user(v-for="u in event.users" :key="u.uid" :user="u")
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, computed } from '@vue/composition-api'
 
 interface Props {
   event: Event
@@ -29,8 +46,22 @@ export default defineComponent({
       required: true
     }
   },
-  setup (props: Props, _context: any) {
+  setup (props: Props, context: any) {
+    const dayjs = context.root.$dayjs
+    const date = computed<Date>(() => props.event.date)
+
+    const year = computed<number>(() => date.value.getFullYear())
+    const month = computed<number>(() => date.value.getMonth() + 1)
+    const day = computed<number>(() => date.value.getDate())
+    const weekChar = computed<number>(() => dayjs(date.value).format('dd'))
+    // const timeRangeText = computed<string>(() => {
+    //   const startTime = dayjs(props.event.startDatetime)
+    // })
     return {
+      year,
+      month,
+      day,
+      weekChar
     }
   }
 })
@@ -38,7 +69,15 @@ export default defineComponent({
 
 <style lang="stylus" scoped>
 .event-card
-  event-card()
-  .description
-    text-truncate()
+  .main
+    display: flex
+    justify-content: space-between
+    margin-bottom: 10px
+  time-info-width = 100px
+  .time-info
+    width: time-info-width
+  .main-info
+    width: 'calc(100% - %s)' % time-info-width
+    .group
+      height: 30px
 </style>
