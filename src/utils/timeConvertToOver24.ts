891ab7b5069ebe3e413ba9a60299cd5aa68999d1
kthatoto <kthatoto@gmail.com>
2020-05-20 01:27:47 +0900

    Add time conveter logic

--- src/utils/timeConvertToOver24.ts
+++ src/utils/timeConvertToOver24.ts
@@ -1,4 +1,8 @@
 import dayjs from 'dayjs'
 
-export default (originDate: Date, time: Date) => {
+export default (originDate: Date, time: Date): string => {
+  const diffDates: number = dayjs(originDate).startOf('day').diff(dayjs(time).startOf('day'), 'day')
+  const calculatedHour: number = parseInt(dayjs(time).format("HH")) + 24 * diffDates
+  const zeroPadding = (num: number) => `0${num}`.slice(-2)
+  return `${calculatedHour}:${dayjs(time).format("mm")}`
 }
